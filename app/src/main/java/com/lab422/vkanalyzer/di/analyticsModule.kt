add7d99ac05270e41b7465fccfa6800805fd096b
Denis Ivanov <den.ivanov@corp.mail.ru>
2020-06-14 21:06:21 +0300

    add facebook sdk and disable

--- app/src/main/java/com/lab422/vkanalyzer/di/analyticsModule.kt
+++ app/src/main/java/com/lab422/vkanalyzer/di/analyticsModule.kt
@@ -3,6 +3,7 @@ package com.lab422.vkanalyzer.di
 import com.lab422.vkanalyzer.utils.analytics.AmplitudeService
 import com.lab422.vkanalyzer.utils.analytics.AppsflyerService
 import com.lab422.vkanalyzer.utils.analytics.CompositeTrackerService
+import com.lab422.vkanalyzer.utils.analytics.FacebookService
 import com.lab422.vkanalyzer.utils.analytics.FirebaseAnalyticsService
 import com.lab422.vkanalyzer.utils.analytics.TrackerService
 import com.lab422.vkanalyzer.utils.analytics.YandexMetricaService
@@ -13,12 +14,14 @@ fun provideAnalyticsModule() = module {
     single { YandexMetricaService() }
     single { FirebaseAnalyticsService() }
     single { AppsflyerService() }
+    single { FacebookService() }
     single<TrackerService> { CompositeTrackerService(
         listOf(
             get(AmplitudeService::class.java),
             get(YandexMetricaService::class.java),
             get(FirebaseAnalyticsService::class.java),
-            get(AppsflyerService::class.java)
+            get(AppsflyerService::class.java),
+            get(FacebookService::class.java)
         )
     ) }
 }
