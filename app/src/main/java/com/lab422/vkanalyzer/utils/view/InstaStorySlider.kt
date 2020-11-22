f6276fdcddb8b9ba5f446b3d1eb552d02fee0f17
Denis Ivanov <den.ivanov@corp.mail.ru>
2020-11-22 15:06:02 +0300

    fix insta story animation

--- app/src/main/java/com/lab422/vkanalyzer/utils/view/InstaStorySlider.kt
+++ app/src/main/java/com/lab422/vkanalyzer/utils/view/InstaStorySlider.kt
@@ -1,11 +1,17 @@
 package com.lab422.vkanalyzer.utils.view
 
+import android.animation.ValueAnimator
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Paint
+import android.graphics.PorterDuff
+import android.graphics.PorterDuffXfermode
 import android.graphics.RectF
 import android.util.AttributeSet
+import android.util.Log
 import android.view.View
+import android.view.animation.LinearInterpolator
+import androidx.core.animation.doOnEnd
 import androidx.core.content.ContextCompat
 import com.lab422.vkanalyzer.R
 import com.lab422.vkanalyzer.utils.extensions.dpToPx
@@ -15,19 +21,41 @@ class InstaStorySlider : View {
     private companion object {
         const val STORY_LINE_INTERVAL = 8f
         const val SIDE_INTERVAL = 12f
+        const val STORY_ANIMATION_DURATION = 5000L
+        const val STORIES_COUNT = 3
     }
 
-    private val bgColor = ContextCompat.getColor(context, R.color.blackAlpha10)
-    private val frontColor = ContextCompat.getColor(context, R.color.blackAlpha20)
+    private val bgColor = ContextCompat.getColor(context, R.color.colorAlto)
+    private val frontColor = ContextCompat.getColor(context, R.color.colorSilver)
+    private val clearColor = ContextCompat.getColor(context, R.color.transparent)
 
     private val storyLineIntervalInPx = context.dpToPx(STORY_LINE_INTERVAL.toInt())
     private val sideIntervalInPx = context.dpToPx(SIDE_INTERVAL.toInt())
 
-    private val bgLine: Paint = Paint().apply {
+    private val bgPaint: Paint = Paint().apply {
         color = bgColor
         style = Paint.Style.FILL_AND_STROKE
         strokeWidth = 10f
     }
+
+    private val frontPaint: Paint = Paint().apply {
+        color = frontColor
+        style = Paint.Style.FILL_AND_STROKE
+        strokeWidth = 10f
+    }
+
+    private val clearPaint: Paint = Paint().apply {
+        color = clearColor
+        style = Paint.Style.FILL_AND_STROKE
+        strokeWidth = 10f
+        xfermode = PorterDuffXfermode(PorterDuff.Mode.DST_OUT)
+    }
+
+    // pair of start and end X coordinates
+    private var storyPoints: List<Pair<Float, Float>>? = null
+    private var storyAnimation: ValueAnimator? = null
+    private var lineOffset: Float = SIDE_INTERVAL
+    private var currentStoryAnimationNumber = 0
 
     constructor(context: Context) : super(context)
     constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
@@ -36,20 +64,41 @@ class InstaStorySlider : View {
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
 
-        calculateStoryPoints(3).forEach { points ->
-            drawLine(canvas, points.first, points.second)
+        if (storyPoints.isNullOrEmpty()) {
+            storyPoints = calculateStoryPoints(STORIES_COUNT)
         }
 
+        // draw bg line
+        drawStaticBackgroundLine(canvas)
+
+        val animationPoints = calculateAnimatedStoryPoints()
+        drawFrontAnimation(canvas, animationPoints.first, lineOffset)
+
+        // start animation if not started yet
+        if (storyAnimation == null) {
+            startLineAnimation(animationPoints.first, animationPoints.second)
+        }
     }
 
-    private fun drawLine(canvas: Canvas, startPoint: Float, endPoint: Float) {
-        canvas.drawRoundRect(RectF(startPoint, 20f, endPoint, 24f), 40f, 40f, bgLine);
+    override fun onDetachedFromWindow() {
+        storyAnimation?.cancel()
+        super.onDetachedFromWindow()
+    }
+
+    private fun drawStaticBackgroundLine(canvas: Canvas) {
+        storyPoints?.forEachIndexed { index, points ->
+            val paint = if (index < currentStoryAnimationNumber) frontPaint else bgPaint
+            drawLine(canvas, points.first, points.second, paint)
+        }
+    }
+
+    private fun drawLine(canvas: Canvas, startPoint: Float, endPoint: Float, paint: Paint) {
+        canvas.drawRoundRect(RectF(startPoint, 20f, endPoint, 24f), 40f, 40f, paint);
     }
 
     private fun calculateStoryPoints(count: Int): List<Pair<Float, Float>> {
         val storyPoints = mutableListOf<Pair<Float, Float>>()
         val storyLineWidth = (width - ((sideIntervalInPx * 2) + (storyLineIntervalInPx * (count - 1)))) / count
-
         var cursor = 0f
 
         for (x in 0 until count) {
@@ -80,12 +129,39 @@ class InstaStorySlider : View {
 
         return storyPoints
     }
+
+    private fun calculateAnimatedStoryPoints(): Pair<Float, Float> {
+        return storyPoints?.get(currentStoryAnimationNumber)!!
+    }
+
+    private fun startLineAnimation(startPosition: Float, finalOffset: Float) {
+        storyAnimation = ValueAnimator.ofFloat(startPosition, finalOffset).apply {
+            addUpdateListener {
+                lineOffset = it.animatedValue as Float
+                postInvalidate()
+            }
+            doOnEnd {
+                onStoryAnimationEnd()
+            }
+            duration = STORY_ANIMATION_DURATION
+            interpolator = LinearInterpolator()
+            start()
+        }
+    }
+
+    private fun drawFrontAnimation(canvas: Canvas, startLinePoint: Float, currentAnimatedPosition: Float) {
+        drawLine(canvas, startLinePoint, currentAnimatedPosition, frontPaint)
+    }
+
+    private fun onStoryAnimationEnd() {
+        if (currentStoryAnimationNumber == STORIES_COUNT - 1) {
+            Log.d("myTag", "Animation has finished")
+            return
+        }
+        currentStoryAnimationNumber++
+
+        storyAnimation?.cancel()
+        storyAnimation = null
+        invalidate()
+    }
 }
-
-
-/*
-val scaleAnim = ObjectAnimator.ofFloat(view_stories_slider, "translationX", 100f)
-scaleAnim.duration = 3000
-// scaleAnim.repeatCount = ValueAnimator.INFINITE
-// scaleAnim.repeatMode = ValueAnimator.REVERSE
-scaleAnim.start()*/
