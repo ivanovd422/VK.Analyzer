146ff702d4b77edbc54d86f4fa8bda125397afcc
Denis Ivanov <den.ivanov@corp.mail.ru>
2021-02-03 22:30:44 +0300

    resolve conflicts

---
+++ analyzerapi/src/test/java/com/lab422/analyzerapi/RegTest.kt
@@ -0,0 +1,30 @@
+package com.lab422.analyzerapi
+
+import kotlin.test.assertTrue
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Test
+
+class FriendsTest : TestBase() {
+
+    @Before
+    fun setUp() = runBlocking {}
+
+    @Test
+    fun `mutual friends should be not empty`() = runBlocking {
+        val result = usersApi.getMutualFriendsId("6492", "2745")
+        assertTrue(result.response.isNullOrEmpty().not(), "should be not empty")
+    }
+
+    @Test
+    fun `friends should be not empty`() = runBlocking {
+        val result = usersApi.getFriendsList().response
+        assertTrue(result.items.isNullOrEmpty().not(), "should be not empty")
+    }
+
+    @Test
+    fun `users list should be not empty`() = runBlocking {
+        val result = usersApi.getUsersByIds("34,243,952,1568").response
+        assertTrue(result.isNullOrEmpty().not(), "should be not empty")
+    }
+}
